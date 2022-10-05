d6c006aaeb1aa7bf4ce43c0c336902607dc9b925
seojeongbin <69031537+seojeongbin@users.noreply.github.com>
2022-10-05 20:57:18 +0900

    .

---
+++ NetworkX/visualize.py
@@ -0,0 +1,26 @@
+# 시각화를 위한 헬퍼함수를 작성한다.
+import torch
+import networkx as nx
+import matplotlib.pyplot as plt
+
+# NX와 Pytorch tensor 모두를 시각화 할 수 있는 함수이다.
+def visualize(h, color, epoch=None, loss=None, accuracy=None):
+    plt.figure(figsize=(7,7))
+    plt.xticks([])
+    plt.yticks([])
+
+    # 데이터가 pytorch일 경우
+    if torch.is_tensor(h):
+        h = h.detach().cpu().numpy()
+        plt.scatter(h[:, 0], h[:, 1], s=140, c=color, cmap="Set2")
+        if epoch is not None and loss is not None and accuracy['train'] is not None and accuracy['val'] is not None:
+            plt.xlabel((f'Epoch: {epoch}, Loss: {loss.item():.4f} \n'
+                       f'Training Accuracy: {accuracy["train"]*100:.2f}% \n'
+                       f' Validation Accuracy: {accuracy["val"]*100:.2f}%'),
+                       fontsize=16)
+    # 데이터가 NX일 경우
+    else:
+        nx.draw_networkx(G, pos=nx.spring_layout(G, seed=42), with_labels=False,
+                         node_color=color, cmap="Set2")
+    plt.show()
+    
